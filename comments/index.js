ec017f76f672d785ffbefd3a003dc715ff0007e7
mikias-tulu <miki.tulu@gmail.com>
2023-08-03 00:00:00 +0000

    Adding comment moderation to comments service

--- comments/index.js
+++ comments/index.js
@@ -20,7 +20,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -29,15 +29,16 @@ app.post('/posts/:id/comments', async (req, res) => {
     data: {
       id: commentId,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: 'pending'
     }
   });
 
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  console.log("Event Received", req.body.type);
+app.post('/events', (req, res) => {
+  console.log('Event Received:', req.body.type);
 
   res.send({});
 });
